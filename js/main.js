5ff5141e9c405e3a3bc6b8c6a341042ccc4f34bf
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-06 12:50:14 +0100

    ok

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let name = prompt("Inserisci il tuo nome");
+let surname = prompt("Inserisci il tuo cognome");
+let color=prompt("Inserisci il tuo colore preferito");
+
+
+document.getElementById("pswd").innerHTML=`${name}${surname}${color}21`;
