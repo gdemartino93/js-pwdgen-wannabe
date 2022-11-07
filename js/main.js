910513e06e236d148c0d0aef2c97d151481f5ac1
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-07 13:05:38 +0100

    cambio nome var deprec

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-let name = prompt("Inserisci il tuo nome");
+let nameof = prompt("Inserisci il tuo nome");
 let surname = prompt("Inserisci il tuo cognome");
 let color=prompt("Inserisci il tuo colore preferito");
 
 
-document.getElementById("pswd").innerHTML=`${name}${surname}${color}21`;
+document.getElementById("pswd").innerHTML=`${nameof}${surname}${color}21`;
